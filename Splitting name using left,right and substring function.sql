adedea244d8ee4d9f45fe21f138bc82b8babf26a
Bondikalla Malyadri <32597556+MalyadriB@users.noreply.github.com>
2023-01-05 13:34:31 +0530

    Update Splitting name using left,right and substring function.sql

--- Splitting name using left,right and substring function.sql
+++ Splitting name using left,right and substring function.sql
@@ -1,10 +1,13 @@
 --Split name using Substring function
-select substring(name_,1,strpos(name_,' ')) as first_name,
-	   substring(name_, strpos(name_,' ')) as last_name
-       
+
+select     substring(name_,1,strpos(name_,' ')) as first_name,
+	   substring(name_, strpos(name_,' ')) as last_name      
 from sample;
 
+
+
 --Split name using Left&Right function
+
 select Left(name_,strpos(name_,' ')-1) as first_name,
        Right(name_,strpos(reverse(name_),' ')-1) as last_name
 from sample;
